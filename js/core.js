d19ccac16db90030036a06a7470b9189d80abeb3
Cal Henderson <cal@iamcal.com>
2010-10-12 00:47:40 -0700

    added new js file for ajax and utilities

---
+++ js/core.js
@@ -0,0 +1,51 @@
+function api_call(method, args, handler){
+
+	args.method = method;
+
+	ajaxify('api.php', args, function(o){
+
+		if (o.ok) return handler(o);
+
+		alert("API error: "+o.error);
+	});
+}
+
+function ajaxify(url, args, handler){
+
+	var req = new XMLHttpRequest();
+	req.onreadystatechange = function(){
+
+		var l_f = handler;
+
+		if (req.readyState == 4){
+			if (req.status == 200){
+
+				this.onreadystatechange = null;
+				eval('var obj = '+req.responseText);
+				l_f(obj);
+			}else{
+				l_f({
+					'ok'	: 0,
+					'error'	: "Non-200 HTTP status: "+req.status,
+					'debug'	: req.responseText
+				});
+			}
+		}
+	}
+
+	req.open('POST', url, 1);
+	//req.setRequestHeader("Method", "POST "+url+" HTTP/1.1");
+	req.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+
+	var args2 = [];
+	for (i in args){
+		args2[args2.length] = encodeURIComponent(i)+'='+encodeURIComponent(args[i]);
+	}
+
+	req.send(args2.join('&'));
+}
+
+function escapeXML(s){
+	s = ""+s;
+	return s.replace(/&/g,"&amp;").replace(/</g,"&lt;").replace(/>/g,"&gt;").replace(/"/g,"&quot;");
+}
