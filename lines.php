b952d23b5eac7089849a7397010363fc3df1bd0e
Cal Henderson <cal@iamcal.com>
2010-10-12 18:42:04 -0700

    show line IDs. syntax cleanup

--- lines.php
+++ lines.php
@@ -8,12 +8,13 @@
 <table border="1" cellpadding="10">
 <? foreach ($ret['rows'] as $row){ ?>
 	<tr>
+		<td><?=$row['id']?></td>
 <? if ($row['has_line']){ ?>
 		<td><div style="height: 4px; width: 100px; background-color: #fff; border-top: 6px solid <?=$row['color']?>; border-bottom: 6px solid <?=$row['color']?>"></div></td>
 <? }else{ ?>
 		<td><div style="height: 16px; width: 100px; background-color: <?=$row['color']?>"></div></td>
 <? } ?>
-		<td><?=$row['name']?>
+		<td><?=$row['name']?></td>
 <?
 	$ret2 = db_fetch("SELECT s.* FROM tube_stations AS s, tube_connections AS c WHERE c.line_id=$row[id] AND (c.station_id_1=s.id OR c.station_id_2=s.id) GROUP BY s.id LIMIT 5;");
 	$names = array();
