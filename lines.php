8cf8591a3cab270fb1d6c8670e053f324d8363e7
Cal Henderson <cal@iamcal.com>
2010-10-21 14:47:28 -0700

    switch the lines page to use smarty

--- lines.php
+++ lines.php
@@ -2,25 +2,26 @@
 	include('include/init.php');
 
 
-	$ret = db_fetch("SELECT * FROM tube_lines");
-?>
+	#
+	# fetch line
+	#
 
-<table border="1" cellpadding="10">
-<? foreach ($ret['rows'] as $row){ ?>
-	<tr>
-		<td><?=$row['id']?></td>
-<? if ($row['has_line']){ ?>
-		<td><div style="height: 4px; width: 100px; background-color: #fff; border-top: 6px solid <?=$row['color']?>; border-bottom: 6px solid <?=$row['color']?>"></div></td>
-<? }else{ ?>
-		<td><div style="height: 16px; width: 100px; background-color: <?=$row['color']?>"></div></td>
-<? } ?>
-		<td><a href="/lines/<?=$row['slug']?>/"><?=$row['name']?></a></td>
-<?
-	$ret2 = db_fetch("SELECT s.* FROM tube_stations AS s, tube_connections AS c WHERE c.line_id=$row[id] AND (c.station_id_1=s.id OR c.station_id_2=s.id) GROUP BY s.id LIMIT 5;");
-	$names = array();
-	foreach ($ret2['rows'] as $row2) $names[] = $row2['name'];
+	$lines = array();
+	$smarty->assign_by_ref('lines', $lines);
+
+	$ret = db_fetch("SELECT * FROM tube_lines");
+	foreach ($ret['rows'] as $row){
+
+		$ret2 = db_fetch("SELECT s.* FROM tube_stations AS s, tube_connections AS c WHERE c.line_id=$row[id] AND (c.station_id_1=s.id OR c.station_id_2=s.id) GROUP BY s.id LIMIT 5;");
+		$row['stations'] = $ret2['rows'];
+
+		$lines[] = $row;
+	}
+
+
+	#
+	# display
+	#
+
+	$smarty->display('page_lines.txt');
 ?>
-		<td><?=implode(', ', $names)?></td>
-	</tr>
-<? } ?>
-</table>
