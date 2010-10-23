0c64bcf87862f37c9897f2e13887dcb63b99ec4e
Cal Henderson <cal@iamcal.com>
2010-10-22 17:04:15 -0700

    just show a few blogs (recent) on the line page, with a separate 'all' page sorting alphabetically

--- line.php
+++ line.php
@@ -14,9 +14,6 @@
 	$line = db_single(db_fetch("SELECT * FROM tube_lines WHERE slug='$slug_enc'"));
 	if (!$line['id']) error_404();
 
-	$line['name'] = str_replace("National Rail - ", "", $line['name']);
-	$line['color'] = $line['color'] ? $line['color'] : 'transparent';
-
 	$smarty->assign('line', $line);
 
 
@@ -33,9 +30,18 @@
 	# fetch weblogs
 	#
 
-	$ret = db_fetch("SELECT w.*, s.station_id FROM tube_weblogs AS w, tube_weblog_stations AS s, tube_connections AS c WHERE w.id=s.weblog_id AND (s.station_id=c.station_id_1 OR s.station_id=c.station_id_2) AND c.line_id=$line[id] GROUP BY w.id ORDER BY w.blog_name ASC");
+	$limit = 10;
+	$has_more = 0;
+
+	$ret = db_fetch("SELECT w.*, s.station_id FROM tube_weblogs AS w, tube_weblog_stations AS s, tube_connections AS c WHERE w.id=s.weblog_id AND (s.station_id=c.station_id_1 OR s.station_id=c.station_id_2) AND c.line_id=$line[id] GROUP BY w.id ORDER BY w.date_create DESC LIMIT ".($limit+1));
+
+	if (count($ret['rows']) == $limit + 1){
+		$has_more = 1;
+		$ret['rows'] = array_slice($ret['rows'], 0, $limit);
+	}
 
 	$smarty->assign('weblogs', $ret['rows']);
+	$smarty->assign('has_more', $has_more);
 
 
 
