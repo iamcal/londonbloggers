fd0d11fc3d100945410777dc9e6451c4c11b755b
Cal Henderson <cal@iamcal.com>
2010-10-28 17:37:57 -0700

    show some stats on the homepage

--- index.php
+++ index.php
@@ -15,6 +15,19 @@
 
 
 	#
+	# counts
+	#
+
+	list($c1) = db_list(db_fetch("SELECT COUNT(*) FROM tube_weblogs"));
+	list($c2) = db_list(db_fetch("SELECT COUNT(*) FROM tube_stations"));
+	list($c3) = db_list(db_fetch("SELECT COUNT(*) FROM tube_lines"));
+
+	$smarty->assign('count_weblogs', $c1);
+	$smarty->assign('count_stations', $c2);
+	$smarty->assign('count_lines', $c3);
+
+
+	#
 	# output
 	#
 
