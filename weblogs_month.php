3b1c24db97ada06d59ae39e5845c7439af6d3f81
Cal Henderson <cal@iamcal.com>
2010-11-01 20:10:58 -0700

    added the weblog month archive page

---
+++ weblogs_month.php
@@ -0,0 +1,35 @@
+<?
+	include("include/init.php");
+
+	loadlib('blogs');
+
+
+	#
+	# get blogs
+	#
+
+	$y = intval($_GET['y']);
+	$m = intval($_GET['m']);
+
+	if ($m < 1 || $m > 12) error_404();
+
+	if ($y < 2000 || $y > 2038) error_404();
+
+	$ts = mktime(0,0,0,$m,1,$y);
+
+	$smarty->assign('display_month', date('F Y', $ts));
+	$smarty->assign('is_future', $ts > time());
+
+	$d = sprintf('%04d-%02d-01', $y, $m);
+
+	$ret = db_fetch("SELECT * FROM tube_weblogs WHERE month_create='$d' ORDER BY blog_name ASC");
+
+	$smarty->assign_by_ref('blogs', $ret['rows']);
+
+
+	#
+	# output
+	#
+
+	$smarty->display('page_weblogs_month.txt');
+?>
