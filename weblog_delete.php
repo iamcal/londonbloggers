48461cdf42f759c34a17b3f3a752b8fecdd94cc1
Cal Henderson <cal@iamcal.com>
2010-11-01 19:05:41 -0700

    added delete-weblog page. give a little more space at the bottom of every page, before the footer

---
+++ weblog_delete.php
@@ -0,0 +1,48 @@
+<?
+	include("include/init.php");
+
+	loadlib('blogs');
+
+
+	#
+	# grab blog
+	#
+
+	$weblog = db_single(db_fetch("SELECT * FROM tube_weblogs WHERE id=".intval($_GET['id'])));
+
+	if (!$weblog['id']) error_404();
+
+	$smarty->assign('weblog', $weblog);
+
+
+	#
+	# check auth
+	#
+
+	if ($_GET['sig'] != blog_signature($weblog['id'])){
+
+		error_404();
+	}
+
+
+
+	#
+	# delete?
+	#
+
+	if ($_POST['done']){
+
+		db_write("DELETE FROM tube_weblogs WHERE id=$weblog[id]");
+		db_write("DELETE FROM tube_weblog_stations WHERE weblog_id=$weblog[id]");
+
+		$smarty->display('page_weblog_delete_done.txt');
+		exit;
+	}
+
+
+	#
+	# output
+	#
+
+	$smarty->display('page_weblog_delete.txt');
+?>
