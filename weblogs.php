fe6de08665ebbaa7cb1bcc198ec3a0c4c07c4257
Cal Henderson <cal@iamcal.com>
2010-11-24 17:35:36 -0800

    added a little sparkline for blogs added over time. why doesn't the chart api allow hiding axes on bar charts?

--- weblogs.php
+++ weblogs.php
@@ -7,9 +7,12 @@
 	#
 
 	$years = array();
+	$series = array();
 
 	$ret = db_fetch("SELECT month_create, COUNT(*) AS num FROM tube_weblogs GROUP BY month_create ASC");
 	foreach ($ret['rows'] as $row){
+
+		$series[] = $row['num'];
 
 		list($y, $m) = explode('-', $row['month_create']);
 
@@ -34,6 +37,45 @@
 	krsort($years);
 
 	$smarty->assign('years', $years);
+
+
+	#
+	# data series
+	#
+
+	array_shift($series); # hide first (huge) value
+	$max = max($series);
+	$chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+
+	$data = '';
+	foreach ($series as $val){
+		$v = floor(61 * $val / $max);
+		$data .= substr($chars, $v, 1);
+	}
+
+
+	#
+	# google chart url
+	#
+
+	$spark_w = 768;
+	$spark_h = 40;
+
+	$params = array(
+		'chs'	=> "{$spark_w}x{$spark_h}",
+		'cht'	=> 'lc:nda',
+		'chco'	=> '113B92',
+		'chd'	=> 's:'.$data,
+		'chm'	=> 'B,113B92,0,0,0',
+	);
+
+	$pairs = array();
+	foreach ($params as $k => $v) $pairs[] = "$k=$v";
+	$spark_url = 'http://chart.apis.google.com/chart?'.implode('&', $pairs);
+
+	$smarty->assign('spark_w', $spark_w);
+	$smarty->assign('spark_h', $spark_h);
+	$smarty->assign('spark_url', $spark_url);
 
 
 	#
